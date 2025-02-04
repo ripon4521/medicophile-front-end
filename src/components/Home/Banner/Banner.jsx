dde308360e3a52db9ac7a5d945997ed670938404
ripon4521 <riponakondo4521@gmail.com>
2025-02-04 18:06:40 +0600

    added animatiooon

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -15,8 +15,10 @@ const Banner = () => {
   }}>
   <div className="hero-overlay "></div>
   <div className="hero-content text-neutral-content text-center">
-    <div data-aos="fade-up" className="">
-    <h1   style={{ fontFamily: 'Rancho, serif' }} className="md:text-7xl lg:text-9xl text-4xl flex gap-2 drop-shadow-md justify-center ">
+    <div  className="">
+    <h1  data-aos="fade-up"
+    data-aos-duration="1000"
+     data-aos-anchor-placement="top-center"  style={{ fontFamily: 'Rancho, serif' }} className="md:text-7xl lg:text-9xl text-4xl flex gap-2 drop-shadow-md justify-center ">
     Welcome to 
 
         
@@ -29,13 +31,17 @@ const Banner = () => {
   }}
 />
             </span> </h1>
-    <h1   style={{ fontFamily: 'Rancho, serif' }} className=" text-xl lg:text-5xl md:text-4xl flex text-center justify-center items-center gap-2 drop-shadow-md"><MdAddCall/>Call Now: <span className="text-yellow-400">07783664574</span> </h1>
+    <h1   data-aos="fade-up"
+     data-aos-offset="100"
+     data-aos-duration="500"
+     data-aos-easing="ease-in-sine"  style={{ fontFamily: 'Rancho, serif' }} className=" text-xl lg:text-5xl md:text-4xl flex text-center justify-center items-center gap-2 drop-shadow-md"><MdAddCall/>Call Now: <span className="text-yellow-400">07783664574</span> </h1>
  
-      <p className="md:mb-5 mb-2 mt-2 max-w-4xl  mx-auto drop-shadow-md lg:text-xl md:text-[20px] text-xs  text-white">
+      <p data-aos="zoom-in"  data-aos-duration="1000" className="md:mb-5 mb-2 mt-2 max-w-4xl  mx-auto drop-shadow-md lg:text-xl md:text-[20px] text-xs  text-white">
       MD Taxi Your trusted transportation partner in the beautiful Pembrokeshire region! Whether you need a quick ride to the local shops, a scenic journey to a coastal destination, or a reliable airport transfer!!
 
       </p>
-      <div className="flex items-center  drop-shadow-md justify-center">
+      <div data-aos="fade-up"
+     data-aos-duration="2000" className="flex items-center  drop-shadow-md justify-center">
           {/* <CiSearch className="text-[1.5rem] cursor-pointer" /> */}
           <button className="md:px-6 md:py-3 px-2 py-2 bg-yellow-400 text-black font-bold drop-shadow-md rounded-md cursor-pointer md:text-xl text-xs">
             Book Now
---
+++ src/components/Home/BookingFrom/BookingFrom.jsx
@@ -0,0 +1,10 @@
+
+const BookingFrom = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default BookingFrom;
