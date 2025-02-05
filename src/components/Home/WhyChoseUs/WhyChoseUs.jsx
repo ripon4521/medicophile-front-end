f6d68699e63fe0123f64d687b84f7d997a75c919
ripon4521 <riponakondo4521@gmail.com>
2025-02-05 20:40:12 +0600

    created why chose use secction and fuully responsive

--- src/components/Home/WhyChoseUs/WhyChoseUs.jsx
+++ src/components/Home/WhyChoseUs/WhyChoseUs.jsx
@@ -7,22 +7,25 @@ const WhyChoseUs = () => {
   
   
     return (
-      <div style={{
+      <div  style={{
         backgroundImage: "url('https://st.depositphotos.com/17620692/61381/v/450/depositphotos_613818782-stock-illustration-black-abstract-banner-background.jpg')",
         backgroundSize: 'cover',
         backgroundPosition: 'center',
-      }} className=" p-6 bg-gray-50 drop-shadow-md shadow h-[650px]">
-  <div className="max-w-7xl mx-auto flex mt-10 gap-20">
-  <div>
+      }} className=" p-6 bg-gray-50 drop-shadow-md shadow lg:h-[650px]">
+  <div className="max-w-7xl mx-auto flex md:mt-10 lg:gap-20 flex-col lg:flex-row">
+  <div data-aos="fade-up"
+     data-aos-duration="2000">
    <p className="text-start text-xl font-semibold drop-shadow-md mb-2 text-yellow-400">Why Chose Us</p>
    <h1 className="text-5xl font-bold  drop-shadow-md text-start text-white">We are dedicated <span className="text-yellow-500">to provide</span> quality service </h1>
-   <img className="w-full -mt-32 -z-20"  src="/src/assets/image-removebg-preview (3).png" alt="" />
+   <img data-aos="fade-up"
+     data-aos-duration="2000" className="w-full lg:-mt-28 -z-20 -mt-20 md:w-[600px]"  src="/src/assets/image-removebg-preview (3).png" alt="" />
    </div>
 
-   <div className="flex flex-col gap-5">
+   <div data-aos="fade-up"
+     data-aos-duration="2000" className="flex flex-col gap-5 -mt-10 md:-mt-16  lg:mt-5 lg:flex-col">
 
 
-    <div className="w-[450px] bg-white p-4 py-2 rounded-2xl shadow ">
+    <div  data-aos-duration="2000" data-aos="fade-left" className="lg:w-[450px] bg-white p-4 py-2 rounded-2xl shadow ">
         <div className="flex items-start gap-5 mt-2">
         <img className="w-20 bg-yellow-400 p-2 rounded-full " src="/src/assets/Taxi-Car.png" alt="" />
       <div>
@@ -38,7 +41,7 @@ const WhyChoseUs = () => {
         
     </div>
 
-    <div className="w-[500px] bg-white p-4 py-2 rounded-2xl shadow ml-10">
+    <div  data-aos-duration="2000" data-aos="fade-left" className="lg:w-[500px] bg-white p-4 py-2 rounded-2xl shadow lg:ml-10">
         <div className="flex items-start gap-5 mt-2">
         <img className="w-20 bg-yellow-400 p-2 rounded-full " src="/src/assets/taxi_driver-removebg-preview.png" alt="" />
       <div>
@@ -54,7 +57,7 @@ const WhyChoseUs = () => {
         
     </div>
 
-    <div className="w-[500px] bg-white p-4 py-2 rounded-2xl shadow ">
+    <div  data-aos-duration="2000" data-aos="fade-left" className="lg:w-[500px] bg-white p-4 py-2 rounded-2xl shadow ">
         <div className="flex items-start gap-5 mt-2">
         <img className="w-20 bg-yellow-400 p-2 rounded-full " src="/src/assets/locationn.png" alt="" />
       <div>
