7d456d2c3fc619b76b37de87bd2c522a03e0c1f3
ripon4521 <riponakondo4521@gmail.com>
2025-02-15 00:50:10 +0600

    updated all

--- src/components/Home/BookingFrom/BookingFrom.jsx
+++ src/components/Home/BookingFrom/BookingFrom.jsx
@@ -4,8 +4,7 @@ import { FaLongArrowAltRight } from "react-icons/fa";
 
 const BookingFrom = () => {
     return (
-        <div data-aos="fade-up"
-            data-aos-duration="2000" className="bg-white shadow lg:max-w-4xl md:max-w-2xl mx-auto px-5 py-5 lg:-mt-32 md:-mt-10 z-90 opacity-90 rounded-2xl "> 
+        <div data-aos="fade-up" data-aos-duration="2000" className="bg-white shadow lg:max-w-4xl md:max-w-2xl mx-auto px-5 py-5 lg:-mt-32 md:-mt-10 z-90 opacity-90 rounded-2xl ">
             <h2 className="text-center text-2xl font-bold drop-shadow-md mb-3">Book Your Ride</h2>
             <hr />
             <form action="" className="flex flex-col mt-4 gap-5">
@@ -13,8 +12,7 @@ const BookingFrom = () => {
                 <div className="flex flex-col md:flex-row gap-5">
                     <div className="w-full md:w-[80%] relative">
                         <span>Pick Up Location</span>
-                        <IoLocationOutline
-                            className=" absolute mt-4 left-3 text-[1.5rem] text-[#777777]"/>
+                        <IoLocationOutline className="absolute mt-4 left-3 text-[1.5rem] text-[#777777]" />
                         <input
                             type="text"
                             name="text"
@@ -26,8 +24,7 @@ const BookingFrom = () => {
 
                     <div className="w-full md:w-[80%] relative">
                         <span>Drop Off Location</span>
-                        <IoLocationOutline
-                            className=" absolute mt-4 left-3 text-[1.5rem] text-[#777777]"/>
+                        <IoLocationOutline className="absolute mt-4 left-3 text-[1.5rem] text-[#777777]" />
                         <input
                             type="text"
                             name="text"
@@ -39,8 +36,7 @@ const BookingFrom = () => {
 
                     <div className="w-full md:w-[80%] relative">
                         <span>Passengers</span>
-                        <GrUserManager
-                            className=" absolute mt-[18px] left-3 text-[1.2rem] text-[#777777]"/>
+                        <GrUserManager className="absolute mt-[18px] left-3 text-[1.2rem] text-[#777777]" />
                         <input
                             type="text"
                             name="text"
@@ -72,23 +68,30 @@ const BookingFrom = () => {
                         />
                     </div>
 
+                    {/* Phone Number input */}
                     <div className="w-full md:w-[80%] relative">
                         <span>Phone Number</span>
                         <input
                             type="tel"
                             name="phone"
                             id="phone"
-                            placeholder="e.g., +8801XXXXXXXXX"
+                            placeholder="e.g., 1XXXXXXXXX"
                             className="peer mt-1 border-border border border-gray-300 rounded-md outline-none pl-10 pr-4 py-3 w-full focus:border-yellow-400 transition-colors duration-300"
                         />
                     </div>
 
-                    <button className="flex items-center hover:gap-2 bg-yellow-400 text-black rounded-2xl px-4 justify-center w-full md:w-[80%] h-14 mt-6 font-bold drop-shadow-md text-xl cursor-pointer ">
-                        Book Taxi <FaLongArrowAltRight className=""/>
-                    </button>
                 </div>
+                     {/* Book Taxi Button */}
+          <div className="flex justify-center">
+          <button className="flex items-center hover:gap-2 bg-yellow-400 text-black rounded-2xl px-4 justify-center w-full md:w-[80%] h-14 mt-3 font-bold drop-shadow-md text-xl cursor-pointer ">
+                Book Taxi <FaLongArrowAltRight className="" />
+            </button>
+          </div>
 
             </form>
+
+       
+
         </div>
     );
 };
